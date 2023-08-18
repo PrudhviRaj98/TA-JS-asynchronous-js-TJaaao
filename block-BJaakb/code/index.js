58c7b26e37ce01529eac5b6856445158058389a1
PrudhviRaj98 <41124774+PrudhviRaj98@users.noreply.github.com>
2023-08-18 11:38:25 -0400

    done

--- block-BJaakb/code/index.js
+++ block-BJaakb/code/index.js
@@ -2,20 +2,19 @@ const input = document.querySelector('.input');
 const parent = document.querySelector('.image-ul');
 
 const searchUrl = (query) => {
-  `https://api.unsplash.com/search/photos?query=${query}&?client_id=A-aavSD688OhvwomhFCsQv114U_oAWuTWeYr5l09CDU`;
+  return `https://api.unsplash.com/search/photos?query=${query}&?client_id=A-aavSD688OhvwomhFCsQv114U_oAWuTWeYr5l09CDU`;
 };
 
 const url =
   'https://api.unsplash.com/photos/random/?client_id=A-aavSD688OhvwomhFCsQv114U_oAWuTWeYr5l09CDU';
 
 function fetch(url) {
+  console.log(url);
   return new Promise((reslove, reject) => {
     xhr = new XMLHttpRequest();
     xhr.open('GET', url);
-    xhr.onload = () => displayUI(reslove(JSON.parse(xhr.response)));
-
+    xhr.onload = () => reslove(JSON.parse(xhr.response));
     xhr.onerror = () => reject('Something went wrong');
-
     xhr.send();
   });
 }
@@ -32,15 +31,19 @@ function displayUI(data) {
   });
 }
 
-fetch(url)
-  .then((userInfo) => displayUI(userInfo))
-  .catch((error) => console.log(error));
+// fetch(url)
+//   .then((userInfo) => {
+//     console.log(userInfo);
+//     displayUI(userInfo);
+//   })
+//   .catch((error) => console.log(error));
 
 function handleInput(event) {
   if (event.keyCode === 13 && input.value) {
     // let value = event.target.value;
     fetch(searchUrl(input.value))
       .then((searchResult) => {
+        console.log(searchResult);
         displayUI(searchResult.results);
       })
       .catch((error) => console.log(error));
