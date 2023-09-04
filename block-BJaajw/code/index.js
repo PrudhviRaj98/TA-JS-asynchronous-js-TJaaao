0a9fc3e445476d25deb2aba230cb6d3f91267dfd
PrudhviRaj98 <41124774+PrudhviRaj98@users.noreply.github.com>
2023-09-04 11:12:42 -0400

    final

--- block-BJaajw/code/index.js
+++ block-BJaajw/code/index.js
@@ -3,9 +3,15 @@ const root = document.querySelector('.old');
 const body = document.querySelector('.body');
 const select = document.querySelector('select');
 
-function createUI(data) {
+function createUI(data, status = false) {
+  //   loader(true);
   let newsCreate = document.createElement('div');
   newsCreate.classList.add('News');
+
+  //   if (status) {
+  //     newsCreate.innerHTML = `<div class="donut"></div>`;
+  //   }
+
   let imageDiv = document.createElement('div');
   imageDiv.classList.add('imageDiv');
   let image = document.createElement('img');
@@ -35,48 +41,79 @@ function createUI(data) {
   innerDiv.append(button);
   newsCreate.append(imageDiv);
   imageDiv.append(image);
-  //   root.innerHTML = '';
 }
 
-data = fetch(url)
-  .then((user) => {
-    if (!user.ok) {
-      body.innerHTML = '';
-      throw new Error(`Error happened ${user.status}`);
-    }
-    return user.json();
-  })
-  .then((data) => {
-    const uniqueValues = new Set();
-    data.forEach((ele) => {
-      uniqueValues.add(ele.newsSite);
-    });
+function loader(status = false) {
+  console.log(status);
+  if (status === true) {
+    root.innerHTML = `<div class="donut"></div>`;
+    //   } else {
+    //     donut.style.display = `none`;
+    //   }
+  }
+}
 
-    uniqueValues.forEach((ele) => {
-      console.log(ele);
-      dropDown(ele);
-    });
-
-    select.addEventListener('change', () => {
-      let value = select.value;
-      root.innerHTML = '';
+function init() {
+  loader(true);
+  data = fetch(url)
+    .then((user) => {
+      if (!user.ok) {
+        body.innerHTML = '';
+        throw new Error(`Error happened ${user.status}`);
+      }
+      return user.json();
+    })
+    .then((data) => {
+      const uniqueValues = new Set();
       data.forEach((ele) => {
-        if (value === ele.newsSite) {
-          createUI(ele);
-        }
+        uniqueValues.add(ele.newsSite);
       });
+
+      uniqueValues.forEach((ele) => {
+        console.log(ele);
+        dropDown(ele);
+      });
+
+      data.forEach((ele) => {
+        createUI(ele);
+      });
+
+      select.addEventListener('change', () => {
+        let value = select.value;
+
+        root.innerHTML = '';
+
+        loader();
+
+        data.forEach((ele) => {
+          if (value === ele.newsSite) {
+            createUI(ele);
+          }
+        });
+      });
+    })
+    .catch((err) => {
+      handleError();
     });
-  })
-  .catch((err) => {
-    let newpara = document.createElement('p');
-    newpara.textContent =
-      'An error occurred while fetching data. Please try again later.';
-    body.append(newpara);
-    console.error(err);
-  });
+  // .finally(() => {
+  //   loader();
+  // });
+}
 
 function dropDown(options) {
   let option = document.createElement('option');
   option.innerText = options;
   select.append(option);
+}
+
+function handleError(
+  message = 'An error occurred due to no internet connection. Please try again later.'
+) {
+  root.innerHTML = message;
+}
+
+if (navigator.onLine) {
+  init();
+} else {
+  handleError();
 }
